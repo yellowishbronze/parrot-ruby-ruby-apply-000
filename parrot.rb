a9768aef9abd73a95aff41cd2f817109ee26107a
Bryan Teng <bteng1226@gmail.com>
2018-06-14 23:12:41 +0000

    automatically backed up by learn

--- parrot.rb
+++ parrot.rb
@@ -1,2 +1,10 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
+def parrot(string)
+if string.length == 0
+  puts ("Squawk!")
+  return ("Squak!")
+end
+puts(string)
+return string
+end
